dc621b947760235e692e963383c4a7ae978d3da6
Saqibkhan15 <aqib12u@gmail.com>
2024-10-12 22:23:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+// var num1 = prompt("enter number");
+// document.write(Math.round(num1) + "<br>");
+// document.write(Math.floor(num1) + "<br>");
+// document.write(Math.ceil(num1) + "<br>");
+// alert(num1);
+
+// var num1 = prompt("enter negative flooting number");
+// num1 = Number(num1);
+// document.write(Math.round(num1) + "<br>");
+// document.write(Math.floor(num1) + "<br>");
+// document.write(Math.ceil(num1) + "<br>");
+// alert(num1);
+
+
+// var diceValue = Math.floor(Math.random() * 6) + 1;
+// document.write("The value of the dice roll is: " + diceValue);
+
+// var coinValue = Math.floor(Math.random() * 2) + 1;
+// var result;
+
+// if (coinValue === 1) {
+//     result = "Heads";
+// } else {
+//     result = "Tails";
+// }
+// document.write("The value of the coin toss is: " + result);
+
+
+// var randomNumber = Math.floor(Math.random() * 100) + 1;
+// alert("The random number between 1 and 100 is: " + randomNumber);
+
+// var weightInput = prompt("Please enter your weight");
+// var weight = parseFloat(weightInput);
+// document.write("Your weight is: " + weight + " kg");
+
+
+// var secretNumber = Math.floor(Math.random() * 10) + 1;
+// var userInput = prompt("Guess the secret number between 1 and 10");
+// userInput = Number(userInput);
+// if (userInput === secretNumber) {
+//     alert("Congratulations! " + secretNumber);
+// } else {
+//     alert("Sorry, try again! ");
+// }
